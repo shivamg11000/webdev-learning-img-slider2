ea880a054ed591b64b4546d6282a1c2011918c18
Shivam Gupta <shivamg11000@gmail.com>
2017-04-24 15:21:09 +0530

    Image slider automatic

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webdev-learning-img-slider2
 Image slider 2 which is automatic.
+
+
+Check the slider https://shivamg11000.github.io/webdev-learning-img-slider2
---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+
+	var i = 0; // Start point
+	var images = [];
+	var timeGap = 1500;
+
+	// Image List
+	images[0] = 'img/image1.jpg';
+	images[1] = 'img/image2.jpg';
+	images[2] = 'img/image3.jpg';
+	images[3] = 'img/image4.jpg';
+
+	// Change Image
+	function changeImg(){
+		document.slide.src = images[i];
+
+		if(i < images.length - 1){
+			i++;
+		}
+		else {
+			i = 0;
+		}
+
+		setTimeout("changeImg()", timeGap);
+	}
+
+	window.onload = changeImg();
